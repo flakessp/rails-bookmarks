2ced26d95446f480ab0def6249a5ffa2e6bc8bac
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2016-10-03 22:51:07 +0300

    created short url

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929132648) do
+ActiveRecord::Schema.define(version: 20161003134754) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "url"
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
 
 end
